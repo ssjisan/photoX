33966d3b9a6727bcd710c1db3e7620256fbf913c
Sadman Sakib Jisan <sadman.sakibjisan@techhack.ca>
2021-12-14 20:28:34 +0600

    mission added

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+$(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position > 500) {
+        $('.nav-menu').addClass('custom-nav');
+    }
+    else {
+        $('.nav-menu').removeClass('custom-nav');
+    }
+});
 
-
-
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+    if (position >= 600) {
+        $('.img-fluid').addClass('img');
+        $('.img-fluid').addClass('text-amination');
+    }
+    else {
+        $('.img-fluid').addClass('img');
+        $('.img-fluid').removeClass('text-amination');
+    }
+})
 
 
 
